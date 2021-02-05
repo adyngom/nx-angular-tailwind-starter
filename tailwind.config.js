0aee419bea635c9351038201b30d1a828a170d25
Ady Ngom <ady.touba.ngom@gmail.com>
2021-02-05 13:35:37 -0500

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './apps/**/*.html',
+    './apps/**/*.ts',
+    './libs/**/*.html',
+    './libs/**/*.ts',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
