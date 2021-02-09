979ff6b23e384883182646649a4556a0a2da639d
Ady Ngom <ady.touba.ngom@gmail.com>
2021-02-08 23:15:54 -0500

    added Angular app

---
+++ apps/tukki/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tukki-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'tukki';
+}
